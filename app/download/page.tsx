33a5c63c2bf7ceae299d707f384351fd01ef568c
Kamran Mushtaq <kaamipresents@gmail.com>
2025-12-06 13:50:38 +0500

    Download Page Mobile Responsive

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -9,9 +9,9 @@ export default function DownloadPage() {
         <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-[7rem]">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-0 items-center max-w-3xl mx-auto">
             {/* Left Side - Content */}
-            <div className="flex flex-col items-center justify-center text-white">
+            <div className="flex flex-col items-center justify-center text-white order-2 lg:order-1">
               {/* Logo */}
-              <div className="relative w-[12vw] h-[12vw] mb-6 sm:mb-2">
+              <div className="relative w-[19.99vw] h-[19.99vw] sm:w-[12vw] sm:h-[12vw] mb-2 mt-2 sm:mt-0 sm:mb-2">
                 <Image
                   src="/images/logos/app-icon.png"
                   alt="Sappnin Logo"
@@ -30,13 +30,13 @@ export default function DownloadPage() {
               {/* Download Button */}
               <Button
                 variant="outline"
-                className="bg-white text-brand-orange hover:bg-white/90 mb-8 px-7 py-[1.2rem] text-[1vw] lg:text-[1vw] rounded-full font-bold"
+                className="bg-white text-brand-orange hover:bg-white/90 mb-4 sm:mb-8 px-7 py-[1.2rem] text-[2.68vw] sm:text-[1vw] rounded-full font-bold"
               >
                 DOWNLOAD NOW
               </Button>
 
               {/* Available On Text */}
-              <p className="text-[1vw] lg:text-[1vw] mb-4 lg:mb-4 text-center">AVAILABLE ON</p>
+              <p className="text-[3.03vw] sm:text-[1vw] mb-0 sm:mb-4 text-center">AVAILABLE ON</p>
 
               {/* Store Buttons */}
                 <DownloadButtons />
@@ -45,9 +45,9 @@ export default function DownloadPage() {
             </div>
 
             {/* Right Side - Phone Mockup */}
-            <div className="flex justify-center">
-              <div className="relative w-full mx-auto lg:max-w-[29.44vw] h-auto">
-                <div className="relative h-[41vw]">
+            <div className="flex justify-center order-1 lg:order-2">
+              <div className="relative w-full mx-auto max-w-[43.18vw] sm:max-w-[29.44vw] h-auto">
+                <div className="relative h-[80.45vw] sm:h-[41vw]">
                   <Image
                     src="/images/download/profile-card.png"
                     alt="Sappnin App"
